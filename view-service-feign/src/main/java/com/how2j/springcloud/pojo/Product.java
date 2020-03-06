3d5b02e5b9f5da7c07a81269822ae2d6efcab3e5
liuguohu <1774687347@qq.com>
2020-03-06 09:13:44 +0800

    断路器hystrix完成

--- view-service-feign/src/main/java/com/how2j/springcloud/pojo/Product.java
+++ view-service-feign/src/main/java/com/how2j/springcloud/pojo/Product.java
@@ -9,4 +9,10 @@ public class Product {
     private int id;
     private String name;
     private int price;
+
+    public Product(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 }
